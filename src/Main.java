89d2b7e7f9953cc9fe35feb8affeaf78eba217bf
bodryjhryap <senya.akimov@mail.ru>
2023-07-07 23:27:38 +0500

    Master branch

--- src/Main.java
+++ src/Main.java
@@ -4,16 +4,16 @@ public class Main {
         System.out.println("levNikolaevichTolstoy.getAuthorName() = " + levTolstoy.getAuthorName());
 
         Author sergeyEsenin = new Author("Сергей Есенин");
-        System.out.println("sergeyEsenin.getAuthorFirstName() = " + sergeyEsenin.getAuthorName());
+        System.out.println("sergeyEsenin.getAuthorName() = " + sergeyEsenin.getAuthorName());
 
         Book warAndPeace = new Book("Война и мир", levTolstoy, 1867);
         System.out.println("warAndPeace.getBookName() = " + warAndPeace.getBookName());
-        System.out.println("warAndPeace.getAuthorName() = " + warAndPeace.getAuthorName());
+        System.out.println("warAndPeace.getAuthorName() = " + warAndPeace.getAuthorName().getAuthorName());
         System.out.println("warAndPeace.getPublishingYear() = " + warAndPeace.getPublishingYear());
 
         Book blackHuman = new Book("Черный человек", sergeyEsenin, 2023);
         System.out.println("blackHuman.getBookName() = " + blackHuman.getBookName());
-        System.out.println("blackHuman.getAuthorName() = " + blackHuman.getAuthorName());
+        System.out.println("blackHuman.getAuthorName() = " + blackHuman.getAuthorName().getAuthorName());
         System.out.println("blackHuman.getPublishingYear() = " + blackHuman.getPublishingYear());
         blackHuman.setPublishingYear(1926);
         System.out.println("blackHuman.getPublishingYear() = " + blackHuman.getPublishingYear());
